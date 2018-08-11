232446020475f37ef7f27b42050477fea4419892
Tjoosten <Topairy@gmail.com>
2018-08-12 00:33:08 +0200

    Fix #6 root = true

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,21 @@
 |
 */
 
-// Welcome routes 
+// Welcome routes - TODO Needs testing
 Route::get('/', 'HomeController@welcome')->name('welcome');
 Route::get('/home', 'HomeController@index')->name('home');
 
-// Account routes
+// Account routes - TODO Needs testing
 Route::get('/account/info', 'Account\InformationSettingsController@index')->name('account.info');
 Route::patch('/account/info' , 'Account\InformationSettingsController@update')->name('account.info.update');
 Route::get('/account/security', 'Account\SecuritySettingsController@index')->name('account.security');
 Route::patch('account/security', 'Account\SecuritySettingsController@update')->name('account.security.update');
 
-// Policy routes
+// Policy routes - TODO Needs testing
 Route::view('/privacy', 'policies/privacy')->name('privacy');
 Route::view('/terms-of-service', 'policies/terms-of-service')->name('terms');
 
-// Users Route
+// Users Route - TODO Needs testing
 Route::get('/users', 'Users\UsersController@index')->name('users.index');
 Route::get('/users/create', 'Users\UsersController@create')->name('users.create');
 Route::get('/users/delete/{user}', 'Users\UsersController@destroy')->name('users.delete');
@@ -34,16 +34,16 @@ Route::get('/users/delete/undo/{user}', 'Users\DestroyController@undo')->name('u
 Route::post('/users/store', 'Users\UsersController@store')->name('users.store');
 Route::get('/users/search', 'Users\UsersController@search')->name('users.search');
 
-// User activation/deactivation routes
+// User activation/deactivation routes - TODO Needs testing
 Route::get('/users/deactivate/{user}', 'Users\ActiveStateController@create')->name('users.deactivate');
 Route::get('/users/activate/{user}', 'Users\ActiveStateController@destroy')->name('users.activate');
 Route::post('/users/deactivate/{user}', 'Users\ActiveStateController@store')->name('users.deactivate.store');
 
-// Activity overview logs 
+// Activity overview logs - TODO Needs testing
 Route::get('/activities', 'ActivityLogs\IndexController@index')->name('activities.index');
 Route::get('/activities/search', 'ActivityLogs\IndexController@search')->name('activities.search');
 
-// Page fragment routes 
+// Page fragment routes - TODO Needs testing
 Route::get('/fragments/status/{fragment}/{status}', 'Fragments\IndexController@status')->name('fragments.status');
 Route::get('/fragments/edit/{fragment}', 'Fragments\IndexController@edit')->name('fragments.edit');
 Route::patch('/fragments/edit/{fragment}', 'Fragments\IndexController@update')->name('fragments.update');
